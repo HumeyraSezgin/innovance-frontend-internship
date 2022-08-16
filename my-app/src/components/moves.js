956d26113d75fada4117354186e019764d76db2a
HumeyraSezgin <hmyra407@gmail.com>
2022-08-16 16:02:25 +0300

    #5 fix path and moves

--- my-app/src/components/moves.js
+++ my-app/src/components/moves.js
@@ -6,13 +6,16 @@ const Moves = ({ history, setXisNext, setStepNumber }) => {
         setXisNext(step % 2 === 0);
     };
 
-    const move = history.map((_step, index) => (
-        <li>
-            <button onClick={() => jumpTo(index)}>
-                {index ? `Go to move # ${index}` : `Go to game start`}
-            </button>
-        </li>
-    ));
-    return <ol>{move}</ol>;
+    return (
+        <ol>
+            {history.map((_step, index) => (
+                <li>
+                    <button onClick={() => jumpTo(index)}>
+                        {index ? `Go to move # ${index}` : `Go to game start`}
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
 }
 export default Moves;

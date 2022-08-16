2d9b499a87d1a466aa20070cc1c3032d00a10336
HumeyraSezgin <hmyra407@gmail.com>
2022-08-16 14:20:56 +0300

    #5 fix move component

--- my-app/src/components/game.js
+++ my-app/src/components/game.js
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 import calculateWinner from "../utils";
 import Board from "./board";
-import moves from './moves'
+import Moves from './moves'
 
 const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(history[stepNumber]);
-  const nextChar = xIsNext ? "X" : "O";
+  const nextPlayer = xIsNext ? "X" : "O";
 
   const handleClick = (i) => {
     const historyPoint = history.slice(0, stepNumber + 1);
     const current = historyPoint[stepNumber];
     const squares = [...current];
     if (winner || squares[i]) return;
-    squares[i] = nextChar;
+    squares[i] = nextPlayer;
     setHistory([...historyPoint, squares]);
     setStepNumber(historyPoint.length);
     setXisNext(!xIsNext);
-  };
-
-  const jumpTo = (step) => {
-    setStepNumber(step);
-    setXisNext(step % 2 === 0);
   };
 
   return (
@@ -34,8 +29,12 @@ const Game = () => {
         </div>
         <div className="game-info">
           <h3>History</h3>
-          {moves()}
-          <h3>{winner ? "Winner: " + winner : "Next Player: " + nextChar}</h3>
+          <Moves
+            history={history}
+            setStepNumber={setStepNumber}
+            setXisNext={setXisNext}
+          />
+          <h3>{winner ? "Winner: " + winner : "Next Player: " + nextPlayer}</h3>
         </div>
       </div>
     </>
--- my-app/src/components/moves.js
+++ my-app/src/components/moves.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const moves = (history, jumpTo) =>
-    history.map((_step, move) => {
-        const destination = move ? `Go to move #${move}` : "Go to Start";
-        return (
-            <li key={move}>
-                <button onClick={() => jumpTo(move)}>{destination}</button>
-            </li>
-        );
-    });
+const Moves = ({ history, setXisNext, setStepNumber }) => {
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0);
+    };
 
-export default moves;
+    const move = history.map((_step, index) => (
+        <li>
+            <button onClick={() => jumpTo(index)}>
+                {index ? `Go to move # ${index}` : `Go to game start`}
+            </button>
+        </li>
+    ));
+    return <ol>{move}</ol>;
+}
+export default Moves;

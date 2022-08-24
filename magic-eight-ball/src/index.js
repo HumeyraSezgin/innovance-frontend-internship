429fd29d7f037bdc57ef6626043e78efdf8b8bc5
HumeyraSezgin <hmyra407@gmail.com>
2022-08-24 15:54:38 +0300

    #10 update handleSubmit-fetch api-move component

--- magic-eight-ball/src/index.js
+++ magic-eight-ball/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-
+import questionAnswer from './components/questionAnswer'
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
 function App() {
@@ -14,16 +14,23 @@ function App() {
     setQuestion(event.target.value);
   }
 
-  function submitting() {
+  async function fetchApi() {
+    let response = await fetch(API_ENDPOINT);
+    let responseData = await response.json();
+    setItem(responseData);
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
     if (question.endsWith('?')) {
       setIsLoaded(true);
       setError(false);
       setQuestion(question);
-      fetch(API_ENDPOINT).then((res) => res.json()).then((data) => setItem(data))
+      fetchApi();
     }
     else {
       setError(true);
-      isLoaded(false);
+      setIsLoaded(false);
     }
   }
 
@@ -31,26 +38,25 @@ function App() {
     <div className="App">
       <h1>Magic Eight Ball</h1>
       <h2>Ask your question?</h2>
-      <input
-        type="text"
-        name="name"
-        onChange={handleOnChange}
-        value={question}
-      />
-      <button type="submit" onClick={submitting}>Submit</button>
-      {error ?
-        <p>You didn't ask a question</p> : null
-      }
-      {isLoaded ?
-        <>
-          <div>{item.answer}</div>
-          <img src={item.image} alt="yes-no-img" />
-        </> : null
-      }
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          onChange={handleOnChange}
+          value={question}
+        />
+        <button type="submit">Submit</button>
+        {error ?
+          <p>You didn't ask a question</p> : null
+        }
+        {isLoaded ?
+          <>
+            {questionAnswer(item,question)}
+          </> : null
+        }
+      </form>
     </div>
   );
-
-
 }
 
 const rootElement = document.getElementById("root");
